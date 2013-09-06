37f3322f2d738c32d0c8cfad31713bdc47fa4e9c
MetaphoricallyHitler <dylanrichmond@gmail.com>
2013-09-06 14:43:04 -0700

    file for local use

---
+++ heroku-set-env.ps1
@@ -0,0 +1,7 @@
+heroku config:set KJVBIBLE=/app/prooftextbot/bibles/kjvapocrypha.p 
+
+heroku config:set NIVBIBLE=/app/prooftextbot/bibles/niv.p
+
+heroku config:set NRSVBIBLE=/app/prooftextbot/bibles/nrsv.p
+
+heroku config:set ESVBIBLE=/app/prooftextbot/bibles/esv.p
